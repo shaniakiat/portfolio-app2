1be7587f5222af4135ed561ffe85fc2c45e25a00
shaniakiat <kiat.shania@gmail.com>
2019-07-12 16:41:37 -0400

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Header from "../components/globals/Header"
-import About from "../components/globals/About"
+import Header from "../components/globals/header"
+import About from "../components/globals/about"
 
 const IndexPage = () => (
   <Layout>
