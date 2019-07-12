9f7107afc201696507f3153cc23f541a77734309
shaniakiat <kiat.shania@gmail.com>
2019-07-12 17:07:24 -0400

    debug

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,47 +12,41 @@ import { StaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 import "./layout.css"
 
-import Navbar from "./globals/navbar.js"
-import Footer from "./globals/footer.js"
+// import Navbar from "./Globals/Navbar"
+// import Footer from "./Globals/Footer"
 
 if (typeof window !== "undefined") {
   // eslint-disable-next-line global-require
   require("smooth-scroll")('a[href*="#"]')
 }
 
-// const Layout = ({ children }) => {
-//   return (
-//     <div>
-//       <Navbar />
-//       {children}
-//       <Footer />
-//     </div>
-//   )
-// }
+const Layout = ({ children }) => {
+  return <div></div>
+}
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <div>
-          <Navbar />
-          {/* <main>{children}</main> */}
-          {children}
-          <Footer />
-        </div>
-      </>
-    )}
-  />
-)
+// const Layout = ({ children }) => (
+//   <StaticQuery
+//     query={graphql`
+//       query SiteTitleQuery {
+//         site {
+//           siteMetadata {
+//             title
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//       <>
+//         <div>
+//           <Navbar />
+//           {/* <main>{children}</main> */}
+//           {children}
+//           <Footer />
+//         </div>
+//       </>
+//     )}
+//   />
+// )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import Layout from "../components/layout.js"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Header from "../components/globals/header.js"
-import About from "../components/globals/about.js"
+// import Header from "../components/Globals/Header"
+// import About from "../components/Globals/About"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Header id="home" class="header" title="Shania Kiat." button="Resume" />
-    <About id="about" class="about" title="About Me" />
+    {/* <Header id="home" class="header" title="Shania Kiat." button="Resume" />
+    <About id="about" class="about" title="About Me" /> */}
   </Layout>
 )
 
