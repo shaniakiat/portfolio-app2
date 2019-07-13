31df4c5ff3a9dd2d1a3f93651619f8c415828fde
shaniakiat <kiat.shania@gmail.com>
2019-07-13 00:41:46 -0400

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Header from "../components/Globals/Header"
+// import Header from "../components/Globals/Header"
 // import About from "../components/Globals/About"
 
 const IndexPage = () => (
