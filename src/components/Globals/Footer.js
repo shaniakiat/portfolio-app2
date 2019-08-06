89040a127f7b439d85a1a446120b38703cc83812
shaniakiat <kiat.shania@gmail.com>
2019-08-06 10:29:15 -0400

    footer

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -2,6 +2,9 @@ import React from "react"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
 import { FaInstagram } from "react-icons/fa"
+import { FaDribbbleSquare } from "react-icons/fa"
+import { FaBehanceSquare } from "react-icons/fa"
+import { FaCodepen } from "react-icons/fa"
 
 export default function Footer() {
   return (
@@ -14,17 +17,24 @@ export default function Footer() {
           <a href="https://github.com/shaniakiat" target="_blank">
             <FaGithub className="github-icon social" />
           </a>
+          <a href="https://dribbble.com/shaniakiat" target="_blank">
+            <FaDribbbleSquare className="dribbble-icon social" />
+          </a>
+          <a href="https://www.behance.net/kshania987f1f" target="_blank">
+            <FaBehanceSquare className="behance-icon social" />
+          </a>
+          <a href="https://codepen.io/shaniakiat/" target="_blank">
+            <FaCodepen className="codepen-icon social" />
+          </a>
           <a href="https://www.instagram.com/shania.took.this/" target="_blank">
             <FaInstagram className="insta-icon social" />
           </a>
         </div>
-        <div className="row">
-          <div className="col-sm-12 mx-auto cold-md-12 text-center text-capitalize">
-            <h3 className="copyright">
-              Designed & Developed by Shania Kiat&nbsp;
-              {new Date().getFullYear()}
-            </h3>
-          </div>
+        <div className="text-center text-capitalize">
+          <h3 className="copyright">
+            Designed & Developed by Shania Kiat&nbsp;
+            {new Date().getFullYear()}
+          </h3>
         </div>
       </div>
     </footer>
