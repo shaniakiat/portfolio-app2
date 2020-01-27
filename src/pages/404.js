084f37d86871dda4d41354b915a5f7233a7962a7
shaniakiat <kiat.shania@gmail.com>
2020-01-27 15:27:11 -0500

    404, layout css

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,9 +8,13 @@ import "../components/Styles/error.css"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1 class="error" align="center">
-      PAGE NOT FOUND
-    </h1>
+    <div class="error" align="center">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <Link to="/#home" className="homeButton">
+        Go Home
+      </Link>
+    </div>
     {/* <p>You just hit a route that doesn&#39;t exist... the sadness.</p> */}
   </Layout>
 )
